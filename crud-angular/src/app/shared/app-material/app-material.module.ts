9eca62936f2a1b635e040ec5dcd052ca96fb0905
lucasGomes01 <gomes.lucasj@gmail.com>
2021-11-11 10:30:40 -0300

    aula 10

--- crud-angular/src/app/shared/app-material/app-material.module.ts
+++ crud-angular/src/app/shared/app-material/app-material.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatToolbarModule } from '@angular/material/toolbar';
-
 
 @NgModule({
   exports: [
     MatCardModule,
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    MatProgressSpinnerModule
   ],
 })
 export class AppMaterialModule { }
