1029771cb8eac8f7989cc39236c5ed606b4a4025
lucasGomes01 <gomes.lucasj@gmail.com>
2021-11-20 09:58:18 -0300

    aula 12
    Pipe para mostrar ícone

--- crud-angular/src/app/shared/app-material/app-material.module.ts
+++ crud-angular/src/app/shared/app-material/app-material.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   exports: [
