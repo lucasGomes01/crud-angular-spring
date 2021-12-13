b482bcf3ed84d31915df2c6a4da49ccb4290114d
lucasGomes01 <gomes.lucasj@gmail.com>
2021-12-13 14:24:33 -0300

    15
    Listar Cursos (API HTTP GET)

--- crud-spring/src/main/java/com/lucas/CrudSpringApplication.java
+++ crud-spring/src/main/java/com/lucas/CrudSpringApplication.java
@@ -1,7 +1,12 @@
 package com.lucas;
 
+import com.lucas.model.Course;
+import com.lucas.repository.CourseRepository;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CrudSpringApplication {
@@ -9,5 +14,17 @@ public class CrudSpringApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CrudSpringApplication.class, args);
 	}
+	
+	@Bean
+	CommandLineRunner initDatabase(CourseRepository courseRepository) {
+		return args -> {
+			courseRepository.deleteAll();
 
+			Course c = new Course();
+			c.setName("Angular");
+			c.setCategory("front-end");
+
+			courseRepository.save(c);
+		};
+	}
 }
---
+++ crud-spring/src/main/java/com/lucas/controller/CourseController.java
@@ -0,0 +1,28 @@
+package com.lucas.controller;
+
+import java.util.List;
+
+import com.lucas.model.Course;
+import com.lucas.repository.CourseRepository;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("/api/courses")
+@AllArgsConstructor
+public class CourseController {
+
+    private CourseRepository courseRepository;
+
+
+    @GetMapping
+    public List<Course> list() {
+        return courseRepository.findAll();
+    }
+
+    
+}
